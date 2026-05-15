8d01751cdd7477e8667bbd9ab0745152448d862c
rlofc <randomlinesofcode@gmail.com>
2026-05-15 19:12:02 +0100

    Fixing the sample debian capsule

--- capsules/debian/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "$CAPSULE_USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-apt-get update && apt-get install -y --no-install-recommends \
-  # fonts-inter \
-  # ..and any other package you want to install on top \
-  # ..of the docker image 
